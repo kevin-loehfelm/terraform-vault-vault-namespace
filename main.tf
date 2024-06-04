769bb61d487e5aae99b0503790053e15927115e7
kevin-loehfelm <kevin.loehfelm@hashicorp.com>
2024-06-04 10:54:26 -0400

    updated metadata

--- main.tf
+++ main.tf
@@ -4,8 +4,17 @@ locals {
 
 resource "vault_namespace" "this" {
   namespace = local.namespace
-  path      = var.name
+  path      = var.path
   custom_metadata = {
-    team = var.metadata_team
+    # Point of contact
+    contact_name  = var.contact_name
+    contact_email = var.contact_email
+    # Description
+    description = var.description
+    # Project context
+    wiki_reference = var.wiki_reference
+    jira_reference = var.jira_reference
+    # Expiry date
+    expiration_date = var.expiration_date
   }
 }
--- variables.tf
+++ variables.tf
@@ -5,12 +5,37 @@ variable "parent_namespace" {
   default     = null
 }
 
-variable "name" {
+variable "path" {
   type        = string
-  description = "namespace name"
+  description = "namespace path"
 }
 
-variable "metadata_team" {
+variable "contact_name" {
   type        = string
-  description = "team name"
+  description = "contact name"
+}
+
+variable "contact_email" {
+  type        = string
+  description = "contact email"
+}
+
+variable "description" {
+  type        = string
+  description = "namespace description"
+}
+
+variable "jira_reference" {
+  type        = string
+  description = "jira url for temporary namespace"
+}
+
+variable "wiki_reference" {
+  type        = string
+  description = "wiki url for temporary namespace"
+}
+
+variable "expiration_date" {
+  type        = string
+  description = "expiration date"
 }
